30a30e8e0cd523236d4d64e1535b835b533c1bf5
onemonday <karakyrt13534@gmail.com>
2022-09-30 19:00:26 +0500

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        try {
-            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            telegramBotsApi.registerBot(new ChannelHandlers());
-            telegramBotsApi.registerBot(new DirectionsHandlers());
-            telegramBotsApi.registerBot(new RaeHandlers());
-            telegramBotsApi.registerBot(new WeatherHandlers());
-            telegramBotsApi.registerBot(new TransifexHandlers());
-            telegramBotsApi.registerBot(new FilesHandlers());
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
+
     }
 }
