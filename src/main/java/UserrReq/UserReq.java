bd78486d3a27ec76ce926de40a28f807a97c4c86
ProduitDeCoton <zalmuxambetov@mail.ru>
2022-09-30 21:09:41 +0500

    Initialization

---
+++ src/main/java/UserrReq/UserReq.java
@@ -0,0 +1,12 @@
+package UserrReq;
+
+public class UserReq {
+    public String start = "/start";
+    public String greet = "Привет";
+    public String help = "Помощь";
+    public String questions = "Задай вопрос";
+    public String cancel = "Отменить";
+    public String back = "Назад";
+    public String yes = "Да";
+    public String no = "Нет";
+}
--- src/main/java/res/Res.java
+++ src/main/java/res/Res.java
@@ -1,12 +1,15 @@
 package res;
 
 public class Res {
-    String Hello = "hello message";
-    String Greeting = "greeting message";
-    String Question = "question message";
-    String Answer = "answer message";
-    String Mistake = "problem message";
-    String bye = "bye message";
+    public String startMessage = "Привет, я чат-бот! Чтобы узнать что я умею, напишите \"Помощь\"";
+    public String helpMessage = """
+            Я умею:
+            -Задать вам вопрос и проверить ваш ответ. Для этого напишите "Задай вопрос"
+            -Ну и здороваться могу, да. Просто напишите "Привет\"""";
+    public String greetingMessage = "Привет)";
+    public String questionMessage1 = "Солнце - это звезда?";
+    public String questionMessage2 = "Земля крутиться вокруг солнца?";
+    public String questionMessage3 = "Луна - это планета?";
 
 
 }
