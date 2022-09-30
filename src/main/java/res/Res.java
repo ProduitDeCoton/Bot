12c6bd2a61113a55f73bf3640da294311c2a6dd5
ProduitDeCoton <zalmuxambetov@mail.ru>
2022-09-30 19:40:00 +0500

    Initialization

---
+++ src/main/java/res/Res.java
@@ -0,0 +1,12 @@
+package res;
+
+public class Res {
+    String Hello = "hello message";
+    String Greeting = "greeting message";
+    String Question = "question message";
+    String Answer = "answer message";
+    String Mistake = "problem message";
+    String bye = "bye message";
+
+
+}
