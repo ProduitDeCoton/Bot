c259fa0e586e6c327af8da98f7cb0d4edf199153
onemonday <karakyrt13534@gmail.com>
2022-10-16 17:40:45 +0500

    Рефакторинг набора команд. Перевод на обычный класс со статическими переменными. Смена названий некоторых методов

---
+++ src/main/java/res/UserCommands.java
@@ -0,0 +1,8 @@
+package res;
+
+public final class UserCommands {
+    public static final String start = "/start";
+    public static final String help = "/help";
+    public static final String startQuiz = "/startquiz";
+    public static final String hello = "/hello";
+}
