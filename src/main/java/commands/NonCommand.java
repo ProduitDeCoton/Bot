9faa82af809c24dd4107908d111d0b92d03c99de
onemonday <karakyrt13534@gmail.com>
2022-10-21 20:17:23 +0500

    Обработка поступающего кода с обработкой исключений

--- src/main/java/commands/NonCommand.java
+++ src/main/java/commands/NonCommand.java
@@ -1,6 +1,13 @@
 package commands;
 
 import exceptions.WrongAuthRedirectUriException;
+import logic.ActiveUsers;
+import org.apache.commons.logging.impl.AvalonLogger;
+import org.telegram.telegrambots.meta.api.objects.User;
+import spotify.exceptions.SpotifyActionFailedException;
+import spotify.exceptions.SpotifyAuthorizationFailedException;
+import spotify.models.errors.SpotifyError;
+import spotify_tools.SpotifySession;
 
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -22,18 +29,23 @@ public class NonCommand {
         throw new WrongAuthRedirectUriException();
     }
 
-    public String nonCommandExecute(Long chatId, String userName, String text) {
+    public String nonCommandExecute(User user, String userName, String text) {
         String answer;
         String code;
         try {
             code = getCode(text);
-            answer = "Всё правильно! Вы молодец";
-        } catch (Exception e) {   //Сделать отдельное исключение
-            answer = e.getMessage() +
-                    "\n\n  Похоже вы неправильно ввели ссылку, попробуйте ещё раз";
-        }
+            SpotifySession session = ActiveUsers.getSession(user);
+            session.setCode(code);
+            session.buildAuthorizationRequestToken();
+            session.buildSpotifyApi();
 
-        catch (Exception e) {
+            ActiveUsers.updateActiveUsers(user, session);
+            answer = session.spotifyApi.getCurrentUser().getDisplayName() + ", вы успешно авторизовались!";
+        } catch (WrongAuthRedirectUriException e) {
+            answer = "Похоже вы неправильно ввели ссылку, попробуйте ещё раз";
+        } catch (SpotifyAuthorizationFailedException | SpotifyActionFailedException e) {
+            answer = "Неверный код. Попробуйте ещё раз.";
+        } catch (Exception e) {
             answer = "Простите, я не понимаю Вас. Похоже, что Вы ввели сообщение, не соответствующее формату. Возможно, Вам поможет /help";
         }
         return answer;
