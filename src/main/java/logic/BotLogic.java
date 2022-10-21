0b0947702c337e90a0c338d9507df83e415b81d4
onemonday <karakyrt13534@gmail.com>
2022-10-21 20:20:33 +0500

    Передача данных о юзере в nonCommandExecute

--- src/main/java/logic/BotLogic.java
+++ src/main/java/logic/BotLogic.java
@@ -41,10 +41,11 @@ public final class BotLogic extends TelegramLongPollingCommandBot{
     @Override
     public void processNonCommandUpdate(Update update) {
         Message msg = update.getMessage();
+        User user = update.getMessage().getFrom();
         Long chatId = msg.getChatId();
         String userName = getUserName(msg);
 
-        String answer = nonCommand.nonCommandExecute(chatId, userName, msg.getText());
+        String answer = nonCommand.nonCommandExecute(user, userName, msg.getText());
         setAnswer(chatId, userName, answer);
     }
 
