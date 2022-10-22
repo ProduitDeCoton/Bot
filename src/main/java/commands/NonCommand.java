f744db3351b5c50ad859a73856c977fb365d0521
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2022-10-23 00:11:06 +0500

    ключевое слово final
    При выполнении метода getCode сущности pattern и matcher не
    меняют своё состояние.

--- src/main/java/commands/NonCommand.java
+++ src/main/java/commands/NonCommand.java
@@ -19,8 +19,8 @@ public class NonCommand {
      * @throws WrongAuthRedirectUriException строка не соответствует шаблону аутентификационного сообщения
      */
     public static String getCode(String authRedirectUri) throws WrongAuthRedirectUriException {
-        Pattern pattern = Pattern.compile("http://localhost:8080/auth/spotify/redirect\\?code=([-_A-Za-z0-9]+)");
-        Matcher matcher = pattern.matcher(authRedirectUri);
+        final Pattern pattern = Pattern.compile("http://localhost:8080/auth/spotify/redirect\\?code=([-_A-Za-z0-9]+)");
+        final Matcher matcher = pattern.matcher(authRedirectUri);
 
         if (matcher.find()) {
             return matcher.group(1);
