5bb8d5624a0130be7b0752a5cb94c7cc603f66da
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2022-10-24 17:35:09 +0500

    написал assert убористее

--- src/test/java/commandsTest/NonCommandTest.java
+++ src/test/java/commandsTest/NonCommandTest.java
@@ -89,7 +89,7 @@ public class NonCommandTest {
         } catch (WrongAuthRedirectUriException e) {
 
         } catch (Exception e) {
-            Assert.assertTrue(false);
+            Assert.fail();
         }
     }
 
