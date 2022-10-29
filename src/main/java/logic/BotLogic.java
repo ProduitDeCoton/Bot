0c36cd661bdc05088e7631858b8b2342a3b9b910
onemonday <karakyrt13534@gmail.com>
2022-10-29 16:20:26 +0500

    Добавлен обработчик inline-команд

--- src/main/java/logic/BotLogic.java
+++ src/main/java/logic/BotLogic.java
@@ -4,15 +4,20 @@ import commands.AuthCommand;
 import commands.HelpCommand;
 import commands.NonCommand;
 import commands.StartCommand;
+import inline_query_commands.GetCurrentPlayingObject;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.meta.api.methods.AnswerInlineQuery;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.inlinequery.result.InlineQueryResult;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
 
-public final class BotLogic extends TelegramLongPollingCommandBot{
+
+public final class BotLogic extends TelegramLongPollingCommandBot {
     private final String BOT_NAME;
     private final String BOT_TOKEN;
     private final NonCommand nonCommand;
@@ -43,13 +48,32 @@ public final class BotLogic extends TelegramLongPollingCommandBot{
      */
     @Override
     public void processNonCommandUpdate(Update update) {
-        Message msg = update.getMessage();
-        User user = update.getMessage().getFrom();
-        Long chatId = msg.getChatId();
-        String userName = getUserName(msg);
+        if (update.hasInlineQuery()) {
+            ArrayList<InlineQueryResult> inlineQueryResults = new ArrayList<>();
+            // Добавлять объекты с результатами ниже
+            inlineQueryResults.add(new GetCurrentPlayingObject().constructInlineQueryResult(update.getInlineQuery().getFrom(),
+                    "Текущий воспроизводимый трек"));
 
-        String answer = nonCommand.nonCommandExecute(user, msg.getText());
-        setAnswer(chatId, userName, answer);
+            AnswerInlineQuery answerInlineQuery = AnswerInlineQuery.builder()
+                    .inlineQueryId(update.getInlineQuery().getId())
+                    .results(inlineQueryResults)
+                    .isPersonal(true)
+                    .build();
+            try {
+                this.execute(answerInlineQuery);
+            } catch (TelegramApiException e) {
+            }
+        } else {
+
+            // Если update не имеет inlineQuery, значит, пользователь отправил код для авторизации
+            Message msg = update.getMessage();
+            User user = update.getMessage().getFrom();
+            Long chatId = msg.getChatId();
+            String userName = getUserName(msg);
+
+            String answer = nonCommand.nonCommandExecute(user, msg.getText());
+            setAnswer(chatId, userName, answer);
+        }
     }
 
     /**
