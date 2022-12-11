150554787a09f1f1e1f41c910be875dd7cc460f4
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2022-12-11 09:00:47 +0500

    c10

--- src/test/java/commandsTest/GroupCommandTest.java
+++ src/test/java/commandsTest/GroupCommandTest.java
@@ -1,34 +1,30 @@
 package commandsTest;
 
-
-import commands.AddCommand;
 import commands.GroupCommand;
 import logic.ActiveGroups;
-import logic.ActiveUsers;
 import org.checkerframework.checker.units.qual.A;
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-
-import commands.NonCommand;
-import exceptions.WrongAuthRedirectUriException;
-import org.mockito.stubbing.OngoingStubbing;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
-import spotifyTools.SpotifyGroup;
+import spotify.models.players.Device;
 import spotifyTools.SpotifySession;
 
 import static org.mockito.Mockito.*;
 
-import commands.GroupCommand;
+import java.util.ArrayList;
 
 public class GroupCommandTest {
     private final SpotifySession session = Mockito.mock(SpotifySession.class, RETURNS_DEEP_STUBS);
 
+    /**
+     * Попытка создания групповой сессии в личном чате.
+     */
     @Test
-    public void test_A() {
+    public void testPrivateChat() {
         final GroupCommand groupCommand = mock(GroupCommand.class);
         final User user = new User(1000L, "Dummy", false);
         final Chat chat = new Chat(1000L, "private");
@@ -37,10 +33,9 @@ public class GroupCommandTest {
         doAnswer(invocation -> {
             String answer = null;
 
-            final String correctAnswer = new StringBuilder()
-                    .append("Похоже, вы пытаетесь создать групповую сессию в личном чате.")
-                    .append("\n\n")
-                    .append("Создайте групповую сессию в чате со своими друзьями.").toString();
+            final String correctAnswer = "Похоже, вы пытаетесь создать групповую сессию в личном чате." +
+                    "\n\n" +
+                    "Создайте групповую сессию в чате со своими друзьями.";
 
             if (chat.getType().equals("private")) {
                 answer = correctAnswer;
@@ -54,24 +49,83 @@ public class GroupCommandTest {
         groupCommand.execute(absSender, user, chat, null);
     }
 
+    /**
+     * Неавторизованный пользователь.
+     */
     @Test
-    public void test_B() {
+    public void testUnauthorizedUser() {
         final GroupCommand groupCommand = mock(GroupCommand.class);
         final User user = new User(1000L, "Dummy", false);
         final Chat chat = new Chat(1000L, "group");
         final AbsSender absSender = Mockito.mock(AbsSender.class, Mockito.CALLS_REAL_METHODS);
 
-        ActiveGroups ag= new ActiveGroups();
+        MockedStatic<ActiveGroups> ag=mockStatic(ActiveGroups.class);
+        ag.when(() -> ActiveGroups.getGroupSession(chat)).thenReturn(null);
+
+        final String correctAnswer = "Пожалуйста, авторизуйтесь в Spotify в личных сообщениях со мной." +
+                "\n\n" +
+                "Для этого введите в чат со мной команду /auth";
 
         doAnswer(invocation -> {
             String answer = null;
-            String correctAnswer = new StringBuilder()
-                    .append("Пожалуйста, авторизуйтесь в Spotify в личных сообщениях со мной.")
-                    .append("\n\n")
-                    .append("Для этого введите в чат со мной команду /auth").toString();
 
-            if (ag.getGroupSession(chat) == null) {
-                answer = correctAnswer;
+            if (chat.getType().equals("private")) {
+                answer = "Похоже, вы пытаетесь создать групповую сессию в личном чате." +
+                        "\n\n" +
+                        "Создайте групповую сессию в чате со своими друзьями.";
+            }
+
+            if (ActiveGroups.getGroupSession(chat) == null) {
+                answer = "Пожалуйста, авторизуйтесь в Spotify в личных сообщениях со мной." +
+                        "\n\n" +
+                        "Для этого введите в чат со мной команду /auth";
+            }
+
+            Assert.assertEquals(correctAnswer, answer);
+            return null;
+
+        }).when(groupCommand).execute(absSender, user, chat, null);
+
+        groupCommand.execute(absSender, user, chat, null);
+    }
+
+    /**
+     * Нет устройства с запущенным спотифай.
+     */
+    @Test
+    public void testNoAvailableDevices() {
+        final GroupCommand groupCommand = mock(GroupCommand.class);
+        final User user = new User(1000L, "Dummy", false);
+        final Chat chat = new Chat(1000L, "group");
+        final AbsSender absSender = Mockito.mock(AbsSender.class, Mockito.CALLS_REAL_METHODS);
+
+        ActiveGroups.createGroup(user, chat);
+
+        when(session.getSpotifyApi().getAvailableDevices().getDevices()).thenReturn(new ArrayList<Device>());
+
+        final String correctAnswer = "Spotify не запущен ни на одном устройстве. " +
+                "Пожалуйста, запустите приложение и повторите попытку.";
+
+        doAnswer(invocation -> {
+            String answer = null;
+
+            if (chat.getType().equals("private")) {
+                answer = "Похоже, вы пытаетесь создать групповую сессию в личном чате." +
+                        "\n\n" +
+                        "Создайте групповую сессию в чате со своими друзьями.";
+            }
+
+            if (ActiveGroups.getGroupSession(chat) == null) {
+                answer = "Пожалуйста, авторизуйтесь в Spotify в личных сообщениях со мной." +
+                        "\n\n" +
+                        "Для этого введите в чат со мной команду /auth";
+            }
+
+            final var devices = session.getSpotifyApi().getAvailableDevices().getDevices();
+
+            if (devices.isEmpty()) {
+                answer = "Spotify не запущен ни на одном устройстве. " +
+                        "Пожалуйста, запустите приложение и повторите попытку.";
             }
 
             Assert.assertEquals(correctAnswer, answer);
